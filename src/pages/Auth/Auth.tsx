f2e2dca96114c096e413cdca62d39cd01304e781
Aidar <asheishenaliev9@gmail.com>
2023-10-18 02:33:01 +0600

    added form validation

--- src/pages/Auth/Auth.tsx
+++ src/pages/Auth/Auth.tsx
@@ -1,15 +1,14 @@
 import { useAppDispatch } from "../../hooks";
 import { addUser } from "../../store";
-import { UserType } from "../../types/index.type";
-import styles from "./Auth.module.scss";
-
 import { useForm, SubmitHandler } from "react-hook-form";
 import { CustomButton } from "../../components";
+import styles from "./Auth.module.scss";
 
 type Inputs = {
   email: string;
   username: string;
   password: string;
+  confirmPassword: string;
 };
 
 export const Auth = () => {
@@ -17,13 +16,14 @@ export const Auth = () => {
     register,
     handleSubmit,
     formState: { errors },
+    getValues,
   } = useForm<Inputs>();
+
+  const dispatch = useAppDispatch();
 
   const onSubmit: SubmitHandler<Inputs> = (data) => {
     dispatch(addUser(data));
   };
-
-  const dispatch = useAppDispatch();
 
   return (
     <div className={styles.auth}>
@@ -32,19 +32,55 @@ export const Auth = () => {
         <input
           type="username"
           placeholder="Имя"
-          {...register("username", { required: true })}
+          {...register("username", { required: true, minLength: 5 })}
         />
+        {errors.username?.type === "minLength" && (
+          <span className={styles.error}>
+            Имя должно содержать не менее 5 символов
+          </span>
+        )}
+
         <input
           type="email"
           placeholder="Email"
-          {...register("email", { required: true })}
+          {...register("email", {
+            required: true,
+            pattern: /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/i,
+          })}
         />
+        {errors.email?.type === "pattern" && (
+          <span className={styles.error}>
+            Введите правильный адрес электронной почты
+          </span>
+        )}
+
         <input
           type="password"
           placeholder="Пароль"
-          {...register("password", { required: true })}
+          {...register("password", { required: true, minLength: 8 })}
         />
-        <input type="password" placeholder="Подвердите пароль" />
+        {errors.password?.type === "minLength" && (
+          <span className={styles.error}>
+            Пароль должен содержать не менее 8 символов
+          </span>
+        )}
+
+        <input
+          type="password"
+          placeholder="Подтвердите пароль"
+          {...register("confirmPassword", { required: true })}
+        />
+        {errors.confirmPassword?.type === "required" && (
+          <span className={styles.error}>Подтвердите пароль</span>
+        )}
+        {getValues("password") !== getValues("confirmPassword") && (
+          <span className={styles.error}>Пароли не совпадают</span>
+        )}
+
+        {/* {Object.keys(errors).length > 0 && (
+          <span className={styles.error}>Заполните все поля</span>
+        )} */}
+
         <CustomButton type="submit">Зарегистрироваться</CustomButton>
       </form>
     </div>
