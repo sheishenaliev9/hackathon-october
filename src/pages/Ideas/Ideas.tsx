48391e9a4e89ae00c1f99b873f2f0c7ea708ace6
Aidar <asheishenaliev9@gmail.com>
2023-10-17 17:05:53 +0600

    made a request to the backend database

--- src/pages/Ideas/Ideas.tsx
+++ src/pages/Ideas/Ideas.tsx
@@ -2,6 +2,7 @@ import { useEffect } from "react";
 import styles from "./Ideas.module.scss";
 import { getIdeas } from "../../store";
 import { useAppDispatch, useAppSelector } from "../../hooks";
+import { IdeasType } from "../../types/index.type";
 
 export const Ideas = () => {
   const { ideas } = useAppSelector((state) => state.ideas);
@@ -17,13 +18,26 @@ export const Ideas = () => {
         <input type="text" />
       </div>
 
-      <div>
-        {
-          ideas.map((idea) => (
-            <div>{idea.title}</div>
-          ))
-        }
+      <div className={styles.ideasList}>
+        {ideas.map((idea: IdeasType) => (
+          <IdeaItem key={idea.id} idea={idea} />
+        ))}
       </div>
+    </div>
+  );
+};
+
+interface IdeaProps {
+  idea: IdeasType;
+}
+
+export const IdeaItem: React.FC<IdeaProps> = ({ idea }) => {
+  const { title, content } = idea;
+
+  return (
+    <div className={styles.idea}>
+      <h2>{title}</h2>
+      <p>{content}</p>
     </div>
   );
 };
--- src/store/actions/IdeasActions.ts
+++ src/store/actions/IdeasActions.ts
@@ -1,11 +1,11 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 
-const IDEAS_URL = "http://localhost:3030";
+const IDEAS_URL = "http://192.168.88.59:8000/api/v1";
 
 export const getIdeas = createAsyncThunk("getIdeas", async () => {
   try {
-    const { data } = await axios.get(`${IDEAS_URL}/ideas`);
+    const { data } = await axios.get(`${IDEAS_URL}/ideas/`);
     return data;
   } catch (error) {
     console.log(error);
