7c1c64d65539c9aefb2eb9dd6c0d2cb1bebb014d
Aibek-Kozonberdiev <ratroniii@gmail.com>
2023-10-19 00:13:57 +0600

    add swagger 2

--- backend_api/config/urls.py
+++ backend_api/config/urls.py
@@ -26,9 +26,9 @@ urlpatterns = [
     path('api/v1/', include('api.urls')),
     path('drf-auth/', include('rest_framework.urls')),
     path('auth/', include('djoser.urls')),
-    re_path(r'^auth/', include('djoser.urls.authtoken')),
     path('swagger<format>/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    re_path(r'^auth/', include('djoser.urls.authtoken')),
 ]
 
 if settings.DEBUG:
