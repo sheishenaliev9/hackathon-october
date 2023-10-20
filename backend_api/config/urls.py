7b119bc3fcbc06ae6ed265f558a27a5b83586be4
xrisent <ormonov31261@gmail.com>
2023-10-20 19:35:10 +0600

    fixed media at debug false

--- backend_api/config/urls.py
+++ backend_api/config/urls.py
@@ -18,6 +18,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include, re_path
+from django.views.static import serve
 
 from .swagger import schema_view
 
@@ -31,5 +32,5 @@ urlpatterns = [
     re_path(r'^auth/', include('djoser.urls.authtoken')),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += [re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT,}),]
